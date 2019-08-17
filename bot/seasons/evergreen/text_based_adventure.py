6b65278d5f16d2782ed3ccf16fab17cf3b8a87d2
glowingrunes <freyab277@gmail.com>
2019-08-17 22:41:42 +0100

    Adding boilerplate code.

---
+++ bot/seasons/evergreen/text_based_adventure.py
@@ -0,0 +1,23 @@
+import logging
+
+from discord.ext import commands
+
+log = logging.getLogger(__name__)
+
+
+class TextBasedAdventure(commands.Cog):
+    """A text-based adventure game."""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def ping(self, ctx):
+        """Pong!"""
+        await ctx.send("Pong!")
+
+
+def setup(bot):
+    """Text-based adventure Cog load."""
+    bot.add_cog(TextBasedAdventure(bot))
+    log.info("TextBasedAdventure cog loaded")
