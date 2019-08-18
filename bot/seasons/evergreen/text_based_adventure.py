3c43532e60a503437ade4d25cdd9743976b19c20
glowingrunes <freyab277@gmail.com>
2019-08-18 21:46:31 +0100

    Got the basic embed reaction functionality working - there is a small glitch with the last "right track" emoji, but that can be fixed later.

--- bot/seasons/evergreen/text_based_adventure.py
+++ bot/seasons/evergreen/text_based_adventure.py
@@ -1,5 +1,5 @@
 import logging
-
+import discord
 from discord.ext import commands
 
 log = logging.getLogger(__name__)
@@ -15,6 +15,61 @@ class TextBasedAdventure(commands.Cog):
     async def ping(self, ctx):
         """Pong!"""
         await ctx.send("Pong!")
+
+    @commands.command()
+    async def embedpages(self, ctx):
+        page1 = discord.Embed(
+            title='Page 1/3',
+            description='Description',
+            colour=discord.Colour.orange()
+        )
+        page2 = discord.Embed(
+            title='Page 2/3',
+            description='Description',
+            colour=discord.Colour.orange()
+        )
+        page3 = discord.Embed(
+            title='Page 3/3',
+            description='Description',
+            colour=discord.Colour.orange()
+        )
+
+        pages = [page1, page2, page3]
+
+        message = await ctx.send(embed=page1)
+
+        await message.add_reaction('\u23ee')
+        await message.add_reaction('\u25c0')
+        await message.add_reaction('\u25b6')
+        await message.add_reaction('\u23ed')
+
+        i = 0
+        emoji = ''
+
+        while True:
+            if emoji == '\u23ee':
+                i = 0
+                await message.edit(embed=pages[i])
+            if emoji == '\u25c0':
+                if i > 0:
+                    i -= 1
+                    await message.edit(embed=pages[i])
+            if emoji == '\u25b6':
+                if i < 2:
+                    i += 1
+                    await message.edit(embed=pages[i])
+            if emoji == '\u23ed':
+                i = 2
+                await message.edit(embed=pages[i])
+
+            res = await ctx.bot.wait_for('reaction_add', timeout=30)
+            if res is None:
+                break
+            if str(res[1]) != 'RetroBot':  # Example: 'MyBot#1111'
+                emoji = str(res[0].emoji)
+                await message.remove_reaction(res[0].emoji, res[1])
+
+        await message.clear_reactions()
 
 
 def setup(bot):
