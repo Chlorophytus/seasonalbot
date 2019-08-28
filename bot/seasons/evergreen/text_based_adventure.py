90cf88ed2173c935380846d54a58ff79fa5b1f72
Roland Metivier <metivier.roland@chlorophyt.us>
2019-08-28 07:18:35 -0500

    Refactor the maze a bit more.

--- bot/seasons/evergreen/text_based_adventure.py
+++ bot/seasons/evergreen/text_based_adventure.py
@@ -17,67 +17,40 @@ class RecursiveDivider:
     class Wall:
         """Randomly generated wall for the maze divider."""
 
-        def __init__(self, horizontal: bool, bounds: [(int, int)]):
+        def __init__(self, horizontal: bool, segment_start: (int, int), segment_end: (int, int),
+                     room_start: (int, int), room_end: (int, int), lazy: bool = True):
             """
-            Make a wall definition and extrapolate values for branching.
+            Make a wall.
 
             :param horizontal: True if horizontal.
-            :param bounds: Bounding box.
-                at 0: The upper left corner.
-                at 1: The lower right corner.
+            :param segment_start: The upper left corner of the line.
+            :param segment_end: The lower-right corner of the line.
+            :param room_start: The upper-left corner of the room.
+            :param room_end: The lower-right corner of the room.
+            :param lazy: If false, a passage is automatically made.
             """
             self.horizontal = horizontal
-            self.bounds = bounds
+            self.segment_start = segment_start
+            self.segment_end = segment_end
+            self.room_start = room_start
+            self.room_end = room_end
 
             if self.horizontal:
-                self.start = (random.randint(self.bounds[0][0], self.bounds[1][0]), self.bounds[0][1])
-                self.end = (self.start[0], self.bounds[1][1])
-                # Horizontal, get a random X to plop a passage in
-                # Remember that walls go left-to-right.
-                # This means that we offset rightward. This is addition.
-                self.passage = random.randint(self.start[0], self.end[0])
-                # Upward is item 0, downward is item 1
-                self.partitions_at = [self.randint_excluded(self.start[0], self.end[0], self.passage) for _ in
-                                      range(2)]
-                self.next_bounds = [
-                    [self.start, (self.partitions_at[0], self.end[1])],
-                    [(self.partitions_at[1], self.end[1]), self.end]
-                ]
+                self.length = self.segment_end[0] - self.segment_start[0]
             else:
-                self.start = (self.bounds[0][0], random.randint(self.bounds[0][1], self.bounds[1][1]))
-                self.end = (self.bounds[1][0], self.start[1])
-                # Vertical, get a random Y coordinate.
-                # Remember that walls go up-to-down.
-                # This means that we offset downward. This is addition.
-                self.passage = random.randint(self.start[1], self.end[1])
-                # Leftward is item 0, rightward is item 1
-                self.partitions_at = [self.randint_excluded(self.start[1], self.end[1], self.passage) for _ in
-                                      range(2)]
-                self.next_bounds = [
-                    [self.start, (self.end[0], self.partitions_at[0])],
-                    [(self.end[0], self.partitions_at[1]), self.end]
-                ]
-            print(f"{self.horizontal} {self.next_bounds}")
+                self.length = self.segment_end[1] - self.segment_start[1]
 
-        @staticmethod
-        def randint_excluded(a: int, b: int, exclusion: int) -> int:
-            """
-            Random integer with an exclusion. The value returned is never the exclusion.
+            if not lazy:
+                self.passage = self.calculate_passage()
 
-            :param a: Minimum.
-            :param b: Maximum.
-            :param exclusion: The value to not generate.
-            :return: A value either between a and `exclusion - 1` or `exclusion + 1` and b.
-            """
-            if random.getrandbits(1):
-                return random.randint(a, exclusion - 1)
-            else:
-                return random.randint(exclusion + 1, b)
+        def calculate_passage(self) -> (int, int):
+            """Calculate a passage, able to be placed into the wall."""
+            return RecursiveDivider.random_position(self.segment_start, self.segment_end)
 
     def __init__(self, done_callback, recursion_max: int, size: (int, int)):
         self.recursion = recursion_max
         self.size = size
-        self.data = [[False] * (size[1])] * (size[0])
+        self.data = [[False] * size[0]] * size[1]
         self.done_callback = done_callback
         self.done = False
 
@@ -89,17 +62,67 @@ class RecursiveDivider:
         else:
             return dimensions[1] > dimensions[0]
 
+    @staticmethod
+    def random_position(a: (int, int), b: (int, int)) -> (int, int):
+        """
+        Determine a random position.
+
+        :param a: Minimum (bounding box, upper left)
+        :param b: Maximum (bounding box, lower right)
+        :return: A random position inside the bounding box formed by `a` and `b`.
+        """
+        if a[0] == b[0]:
+            return a[0], random.randint(a[1], b[1])
+        elif a[1] == b[1]:
+            return random.randint(a[0], b[0]), a[1]
+        else:
+            return random.randint(a[0], b[0]), random.randint(a[1], b[1])
+
+    def split_wall(self, parent: Wall, left_side_relative: bool):
+        """
+        Split the specified wall.
+
+        :param parent: The parent wall.
+        :param left_side_relative:
+            If a horizontal parent:
+                - split upward if true.
+                - else split downward.
+            If a vertical parent:
+                - split leftward if true.
+                - else split rightward.
+        """
+        passage = None
+        wall = None
+        # TODO
+        if left_side_relative:
+            # NOTE: The passage variable can signify the next wall's placement.
+            passage = self.random_position(parent.room_start, parent.segment_end)
+            if parent.horizontal:
+                wall = RecursiveDivider.Wall(False, (passage[0], parent.room_start[1]),
+                                             (passage[0], parent.segment_end[1]),
+                                             parent.room_start, (passage[0], parent.room_end[1]))
+            else:
+                pass
+        else:
+            passage = self.random_position(parent.segment_start, parent.room_end)
+
+        wall.passage = passage
+        return wall
+
     async def start(self):
         """Start dividing the maze."""
         horizontal = self.determine_orientation(self.size)
-        await self.recurse(RecursiveDivider.Wall(horizontal, [
-            (0, 0),
-            self.size
-        ]))
+        seed_position = self.random_position((0, 0), self.size)
+        if horizontal:
+            await self.recurse(RecursiveDivider.Wall(True, (0, seed_position[1]), (self.size[0], seed_position[1]),
+                                                     (0, 0), self.size, False))
+        else:
+            await self.recurse(RecursiveDivider.Wall(False, (seed_position[0], 0), (seed_position[0], self.size[1]),
+                                                     (0, 0), self.size, False))
 
     async def recurse(self, parent: Wall):
-        """Do not call this method. Recurses steps through the maze."""
-        if self.recursion < 1:
+        """Recursively divides the maze."""
+        if self.recursion < 1 or parent.length < 4:
             if not self.done:
                 self.done = True
                 self.done_callback(self)
@@ -107,14 +130,10 @@ class RecursiveDivider:
         self.recursion -= 1
 
         if parent.horizontal:
-            for i in range(parent.start[0], parent.end[0]):
-                print(f"{parent.start} {i}")
-        else:
-            for i in range(parent.start[1], parent.end[1]):
-                print(f"{parent.start} {i}")
 
-        await self.recurse(RecursiveDivider.Wall(not parent.horizontal, parent.next_bounds[0]))
-        await self.recurse(RecursiveDivider.Wall(not parent.horizontal, parent.next_bounds[1]))
+            pass
+        else:
+            pass
 
 
 class TextBasedAdventure(commands.Cog):
